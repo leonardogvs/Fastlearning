5339a1c82a0af8f3a454c037c958f304fbff0f79
leonardogvs <leo.velho.soaress@gmail.com>
2024-07-19 19:39:11 +0100

    Printing predefined age and name using int and str

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,5 +1,14 @@
 public class Exercise_1 {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        //Create an int variable for the age number.
+        int age = 30;
+        //Create a String variable for the name.
+        String name = "Bob";
+
+        //Print out names by Greeting user, welcoming to java and telling them their age.
+        System.out.print("Hello, " + name + "!");
+        System.out.println("Welcome to Java!");
+        System.out.print("You are " + age + " years old.");
+
     }
 }
