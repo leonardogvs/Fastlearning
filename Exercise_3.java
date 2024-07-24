5b2f8c258555c8d04aa66ee98ec8f7ec696257e2
leonardogvs <leo.velho.soaress@gmail.com>
2024-07-24 23:22:58 +0100

    Just testing tricky simple prints

---
+++ Exercise_3.java
@@ -0,0 +1,6 @@
+public class Exercise_3 {
+    public static void main(String[] args) {
+        System.out.println(" System.out.println(" + "); ");
+        System.out.println((-5) * (-10) * (-20));
+    }
+}
